8ff60bac70eb4a9a09346e7e2d1529650a1b2b1a
Owolabi shinaayomi <koredeowolabi62@gmail.com>
2025-06-28 22:53:53 +0100

    model and migration of specifc table
    created the model and migration of the independent tables available

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,8 +15,11 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('phone_number');
+            $table->string('address');
+            $table->string('password')->bcrypt();
+            $table->timestamp('last_login');
+           $table->enum('status', ['active', 'unverified', 'suspended'])->default('unverified');
             $table->rememberToken();
             $table->timestamps();
         });
