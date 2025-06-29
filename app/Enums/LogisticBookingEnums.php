75dc5dc2de3056b116a7e774819ba29edf0605d9
Willy-sam021 <s.o.williams021@gmail.com>
2025-06-29 14:00:57 +0100

    Created logistc Enum
    created the logistic booking enum

---
+++ app/Enums/LogisticBookingEnums.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Enums;
+
+enum LogisticBookingEnums
+{
+     //
+    const DRAFT = "draft";
+    const CONFIRMED = 'confirmed';
+    const SHIPPED = 'shipped';
+    const ARRIVED = 'arrived';
+    const DELIVERED = 'delivered';
+}
