7eb2986c75d4884382dc87a0c41c9aaa9df223cf
Absamis <smallkid1@yahoo.com>
2025-06-30 14:16:56 +0100

    Signup and login endpoint added

---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'name' => $this->name,
+            'email' => $this->email,
+            'address' => $this->address,
+            'phone_number' => $this->phone_number,
+            'access_token' => $this->token,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at
+        ];
+    }
+}
