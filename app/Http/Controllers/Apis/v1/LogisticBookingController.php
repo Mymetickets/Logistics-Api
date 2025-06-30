d6dc87f2c7b77e0905b78a7494d6215d6da64fe2
Willy-sam021 <s.o.williams021@gmail.com>
2025-06-30 15:29:45 +0100

    updated the logistic controller
    imported the necessary classes

--- app/Http/Controllers/Apis/v1/LogisticBookingController.php
+++ app/Http/Controllers/Apis/v1/LogisticBookingController.php
@@ -4,10 +4,11 @@ namespace App\Http\Controllers\Apis\v1;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Http\Requests\LogisticBookingRequest;
+use App\Http\Requests\Bookings\StoreLogisticBookingRequest;
+use App\Http\Requests\Bookings\UpdateLogisticBookingRequest;
 use App\Services\LogisticBookingServices;
 
 class LogisticBookingController extends Controller
 {
-    
+
 }
