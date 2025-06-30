776f055e6861ac454d5dc2bd3839e24e1594c3f5
Willy-sam021 <s.o.williams021@gmail.com>
2025-06-30 14:09:29 +0100

    Created the Service and controller
    created the logistic booking controller and the services

---
+++ app/Services/LogisticBookingServices.php
@@ -0,0 +1,52 @@
+<?php
+namespace App\Services;
+
+use App\Repositories\LogisticBookingRepository;
+
+class LogisticBookingServices
+{
+    protected $logisticBookingRepository;
+
+    public function __construct(LogisticBookingRepository $logisticBookingRepository)
+    {
+        $this->logisticBookingRepository = $logisticBookingRepository;
+    }
+
+    public function createBooking(array $data)
+    {
+        return $this->logisticBookingRepository->create($data);
+    }
+
+    public function updateBooking($id, array $data)
+    {
+        return $this->logisticBookingRepository->update($id, $data);
+    }
+
+    public function getBookingById($id)
+    {
+        return $this->logisticBookingRepository->findById($id);
+    }
+
+    public function deleteBooking($id)
+    {
+        return $this->logisticBookingRepository->delete($id);
+    }
+    public function getAllBookings()
+    {
+        return $this->logisticBookingRepository->all();
+    }
+
+
+    public function getBookingsByStatus($status)
+    {
+        return $this->logisticBookingRepository->findByStatus($status);
+    }
+    public function getBookingsByUserId($userId)
+    {
+        return $this->logisticBookingRepository->findByUserId($userId);
+    }
+
+}
+
+
+?>
