9f4f2c3c3b6554619ab782811f3e27aa1851dee4
Willy-sam021 <s.o.williams021@gmail.com>
2025-07-01 22:48:41 +0100

    updated the logistic booking route
    added the middle ware

--- routes/api/v1/logisticBooking.php
+++ routes/api/v1/logisticBooking.php
@@ -3,10 +3,13 @@
 use App\Http\Controllers\Apis\v1\LogisticBookingController;
 use Illuminate\Support\Facades\Route;
 
-Route::post("/booking-create", [LogisticBookingController::class, "createBooking"]);
-Route::put("/booking-update/{id}", [LogisticBookingController::class, "updateBooking"]);
-Route::get("/booking/{id}", [LogisticBookingController::class, "getBookingById"]);
-Route::delete("/booking-delete/{id}", [LogisticBookingController::class, "deleteBooking"]);
-Route::get("/bookings", [LogisticBookingController::class, "getAllBookings"]);
-Route::get("/bookings/user", [LogisticBookingController::class, "getBookingsByUserId"]);
-Route::post("/bookings/search", [LogisticBookingController::class, "searchBookings"]);
+// Define API routes for logistic bookings
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::post("/booking-create", [LogisticBookingController::class, "createBooking"]);
+    Route::put("/booking-update/{id}", [LogisticBookingController::class, "updateBooking"]);
+    Route::get("/booking/{id}", [LogisticBookingController::class, "getBookingById"]);
+    Route::delete("/booking-delete/{id}", [LogisticBookingController::class, "deleteBooking"]);
+    Route::get("/bookings", [LogisticBookingController::class, "getAllBookings"]);
+    Route::get("/bookings/user", [LogisticBookingController::class, "getBookingsByUserId"]);
+    Route::post("/bookings/search", [LogisticBookingController::class, "searchBookings"]);
+});
