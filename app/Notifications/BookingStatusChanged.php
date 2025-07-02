4e6ab757c0ea166ab00b55c9d5547f02cee59531
Willy-sam021 <s.o.williams021@gmail.com>
2025-07-02 15:05:17 +0100

    created the notification class
    created a notification class for when a booking status is changed

---
+++ app/Notifications/BookingStatusChanged.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class BookingStatusChanged extends Notification
+{
+    use Queueable;
+
+    protected $booking;
+
+    public function __construct($booking)
+    {
+        $this->booking = $booking;
+    }
+
+    public function via($notifiable)
+    {
+        return ['database'];
+    }
+
+    public function toDatabase($notifiable)
+    {
+        return [
+            'booking_id' => $this->booking->id,
+            'status' => $this->booking->status,
+            'message' => 'Your booking status changed to ' . $this->booking->status,
+        ];
+    }
+}
