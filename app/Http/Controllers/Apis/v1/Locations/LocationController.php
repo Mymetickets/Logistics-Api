779edd0123610965fffb714791f89576b372a258
Willy-sam021 <s.o.williams021@gmail.com>
2025-07-05 20:31:00 +0100

    created the folders
    created the services, repositories and controller for state, country and location

---
+++ app/Http/Controllers/Apis/v1/Locations/LocationController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Apis\v1\Locations;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LocationController extends Controller
+{
+    //
+}
