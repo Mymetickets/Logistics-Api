953f649ec1497d7454cae63dec131f4bd9ef1b1d
Willy-sam021 <s.o.williams021@gmail.com>
2025-07-05 21:30:19 +0100

    updated the state repository
    added the methods

---
+++ app/Repositories/Locations/StateRepository.php
@@ -0,0 +1,48 @@
+<?php
+namespace App\Repositories\Locations;
+use App\Models\Locations\State;
+
+class StateRepository implements IRepository
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function all()
+    {
+        return State::query()->paginate();
+    }
+
+    public function findById($id)
+    {
+        return State::find($id);
+    }
+
+    public function create($data)
+    {
+        return State::create($data);
+    }
+
+    public function update($id, $data)
+    {
+        return State::where("id", $id)->update($data);
+    }
+
+    public function delete($id)
+    {
+        return State::where("id", $id)->delete();
+    }
+
+    public function search($param)
+    {
+        return State::query()
+            ->where("name", "LIKE", "%$param%")
+            ->orWhere("code", "LIKE", "%$param%")
+            ->paginate();
+    }
+}
+?>
