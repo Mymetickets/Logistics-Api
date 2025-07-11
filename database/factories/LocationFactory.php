6c227ceecff0d643f4a925c61b6d13dae0e51788
Willy-sam021 <s.o.williams021@gmail.com>
2025-07-11 18:30:12 +0100

    created factory
    created the country and state factories

--- database/factories/LocationFactory.php
+++ database/factories/LocationFactory.php
@@ -3,6 +3,9 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Locations\State;
+use App\Models\Locations\Country;
+use App\Models\Locations\Location;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Location>
@@ -17,7 +20,10 @@ class LocationFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'city'        => fake()->city,
+            'country_id'  => Country::factory(), // or manually set an ID
+            'state_id'    => State::factory(),   // or manually set an ID
+            'status'      => fake()->boolean() ? 'active' : 'inactive', // example statuses
         ];
     }
 }
